77281bd2cd2167a6cfba66a422e9e0687fe9d09d
Vadim Goncharov <vg@vadimgoncharov.ru>
2017-11-26 23:56:56 +0300

    [TotalEvenOdd] Change navSection animation to flip

--- src/shared/ui/TotalEvenOdd/TotalEvenOddNavSection.tsx
+++ src/shared/ui/TotalEvenOdd/TotalEvenOddNavSection.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
-import * as gsap from 'gsap';
-import {TweenLite} from 'gsap';
-const OrelImg = require('./navSectionOrel.jpg') as string;
-const ReshkaImg = require('./navSectionReshka.jpg') as string;
+import * as OrelImg from './navSectionOrel.jpg';
+import * as ReshkaImg from './navSectionReshka.jpg';
 
 type TProps = {
   triggerAnimation: boolean,
@@ -10,67 +8,30 @@ type TProps = {
 
 export default class TotalEvenOddNavSection extends React.Component<TProps, void> {
   private rootEl: HTMLElement;
-  private orelImg: HTMLDivElement;
-  private reshkaImg: HTMLDivElement;
-  private tween: TweenLite;
-  private obj = {value: 1};
 
   public componentWillReceiveProps(nextProps: TProps) {
     if (this.props.triggerAnimation !== nextProps.triggerAnimation) {
       if (nextProps.triggerAnimation) {
-        this.tween.play();
+        this.rootEl.classList.add('is-hover');
       } else {
-        this.tween.reverse();
+        this.rootEl.classList.remove('is-hover');
       }
     }
   }
 
-  public componentDidMount() {
-    const {orelImg, reshkaImg} = this;
-    reshkaImg.style.opacity = '0';
-    this.tween = new TweenLite(this.obj, 0.3, {
-      value: 10,
-      paused: true,
-      ease: gsap.Linear.ease,
-      onUpdate: () => {
-        if (Math.round(this.obj.value) % 2 === 1) {
-          TweenLite.set(orelImg, {opacity: 1});
-          TweenLite.set(reshkaImg, {opacity: 0});
-        } else {
-          TweenLite.set(orelImg, {opacity: 0});
-          TweenLite.set(reshkaImg, {opacity: 1});
-        }
-      },
-    });
-  }
-
   public render() {
     return (
-      <div className="NavSection" ref={this.onRefSet} style={{position: 'relative'}}>
+      <div className="NavSection is-totalEvenOdd" ref={this.onRefSet}>
         <div
           className="NavSectionImg is-orel"
           style={{
             backgroundImage: `url(${OrelImg})`,
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat',
-            width: '101px',
-            height: '100px',
-            position: 'absolute',
-            left: '0',
-            top: '0',
           }}
         />
         <div
           className="NavSectionImg is-reshka"
           style={{
             backgroundImage: `url(${ReshkaImg})`,
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat',
-            width: '101px',
-            height: '100px',
-            position: 'absolute',
-            left: '0',
-            top: '0',
           }}
         />
       </div>
@@ -79,7 +40,5 @@ export default class TotalEvenOddNavSection extends React.Component<TProps, void
 
   private onRefSet = (el: HTMLElement) => {
     this.rootEl = el;
-    this.orelImg = el.querySelector('.is-orel') as HTMLDivElement;
-    this.reshkaImg = el.querySelector('.is-reshka') as HTMLDivElement;
   };
 }
