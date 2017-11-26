1d036a54c2eeaea45f68a5489f7d29f5a4fe5a1a
Vadim Goncharov <vg@vadimgoncharov.ru>
2017-11-26 23:56:13 +0300

    Add image types typescript declaration
    Now we can import images using "import" not "require"

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,4 +1,8 @@
 declare module 'react-hot-loader';
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.svg';
 
 interface RequireImport {
     default: any;
