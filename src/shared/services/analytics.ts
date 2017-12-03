0a2042546a61baa8d8ba28c88aee28b6142cdbe1
Vadim Goncharov <vg@vadimgoncharov.ru>
2017-12-03 15:34:29 +0300

    [services/analytics] Remove yaMetrika warning

--- src/shared/services/analytics.ts
+++ src/shared/services/analytics.ts
@@ -5,8 +5,6 @@ type TYaCounter = {
   reachGoal: TReachGoal,
 };
 
-let wasGettingYaCounterError = false;
-
 function getYaCounter(): TYaCounter | null {
   const id = c.analytics.YANDEX_METRIKA_COUNTER_ID;
   const yaCounterName = `yaCounter${id}`;
@@ -17,10 +15,6 @@ function getYaCounter(): TYaCounter | null {
 function reachYaGoal(goalId: string, params?: object, callback?: () => void, ctx?: object): void {
   const yaCounter = getYaCounter();
   if (!yaCounter) {
-    if (!wasGettingYaCounterError) {
-      console.error(`Yandex.Metrika with id="${c.analytics.YANDEX_METRIKA_COUNTER_ID}" not found`);
-      wasGettingYaCounterError = true;
-    }
     return;
   }
   yaCounter.reachGoal(goalId, params, callback, ctx);
