284fa55f728485012215f0a1432256442455a038
Vadim Goncharov <vg@vadimgoncharov.ru>
2017-12-03 11:55:26 +0300

    [Footer] Add roles to authors and change styles

--- src/shared/ui/Footer/Footer.tsx
+++ src/shared/ui/Footer/Footer.tsx
@@ -1,55 +1,68 @@
 import * as React from 'react';
 import * as classNames from 'classnames';
-import Link from '../Link';
-import {getRandomInt} from 'shared/utils/random';
 import './Footer.scss';
 
 type TAuthor = {
   name: string,
+  role?: string,
   date: number,
   isCurrMaintainer: boolean,
   link?: string,
 };
 const AUTHORS: TAuthor[] = [
   {
+    name: 'Людвиг Быстроновский',
+    date: 2006,
+    isCurrMaintainer: true,
+    link: '/',
+  },
+  {
     name: 'Вадим Гончаров',
+    role: 'Дизайнер-технолог',
     date: 2017,
     isCurrMaintainer: true,
     link: 'http://vadimgoncharov.ru',
   },
   {
     name: 'Лёля Ле',
+    role: 'Технический дизайнер',
     date: 2017,
     isCurrMaintainer: true,
     link: 'http://leindustrial.ru',
   },
   {
+    name: 'Стас Щербаков',
+    role: 'Фотограф',
+    date: 2017,
+    isCurrMaintainer: false,
+  },
+  {
     name: 'Алексей Анисимов',
+    role: 'Технолог',
     date: 2015,
     isCurrMaintainer: true,
     link: 'http://for-fun-dev.com',
   },
   {
     name: 'Андрей Шитов',
+    role: 'Технолог',
     date: 2007,
     isCurrMaintainer: false,
     link: 'http://shitov.ru',
   },
   {
     name: 'Сергей Муратов',
+    role: 'Технолог',
     date: 2006,
     isCurrMaintainer: false,
   },
 ];
-
-const MAIL_WORDS: string[] = ['почта', 'почва', 'почка'];
 
 export default class Footer extends React.Component<any, any> {
   public render() {
     return (
       <section className="Footer">
         {this.renderAuthors()}
-        {this.renderContacts()}
       </section>
     );
   }
@@ -57,12 +70,9 @@ export default class Footer extends React.Component<any, any> {
   private renderAuthors() {
     return (
       <div className="Footer-authors">
-        <div className="Footer-authorsOwner">
-          Сайт откроется <a className="Link" href="/">Людвигом Быстроновским</a>
-        </div>
-        <div className="Footer-authorsDevs">
-          <span className="Footer-authorsDevsTitle">Над сайтооткрыванием работали:</span>
-          <ol className="Footer-authorsDevsItems">
+        <div className="Footer-authorsTitle">Над сайтооткрыванием работали</div>
+        <div className="Footer-authorsItemsItemsContainer">
+          <ol className="Footer-authorsItems">
             {AUTHORS.map(this.renderItem)}
           </ol>
         </div>
@@ -71,12 +81,12 @@ export default class Footer extends React.Component<any, any> {
   }
 
   private renderItem = (item: TAuthor, index: number) => {
-    const devsItemClassName = classNames('Footer-authorsDevsItem', {
+    const devsItemClassName = classNames('Footer-authorsItem', {
       'is-currMaintainer': item.isCurrMaintainer,
     });
-    const {name, date, link} = item;
+    const {name, role, date, link} = item;
     let devName = (
-      <span className="Footer-authorsDevsItemName">{name}</span>
+      <div className="Footer-authorsItemName">{name}</div>
     );
     if (link) {
       devName = (
@@ -85,18 +95,12 @@ export default class Footer extends React.Component<any, any> {
     }
     return (
       <li className={devsItemClassName} key={index}>
-        {devName}
-        <span className="Footer-authorsDevsItemDate">{date}</span>
+        <div className="Footer-authorsItemLayout">
+          {devName}
+          {Boolean(role) && <div className="Footer-authorsItemRole">{role}</div>}
+          <div className="Footer-authorsItemDate">{date}</div>
+        </div>
       </li>
     );
   };
-
-  private renderContacts() {
-    const mailWord = MAIL_WORDS[getRandomInt(0, MAIL_WORDS.length - 1)];
-    return (
-      <div className="Footer-contacts">
-        Эл. {mailWord}: <Link href="mailto:lu@ldwg.ru">lu@ldwg.ru</Link>
-      </div>
-    );
-  }
 }
